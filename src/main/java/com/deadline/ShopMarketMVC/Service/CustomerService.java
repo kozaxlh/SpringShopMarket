94b90343caa1f8e43c6b1dc8be8dbdac875f0c0f
Admin <Admin@Admin-PC>
2023-05-08 21:11:49 +0700

    add check login Service

--- src/main/java/com/deadline/ShopMarketMVC/Service/CustomerService.java
+++ src/main/java/com/deadline/ShopMarketMVC/Service/CustomerService.java
@@ -1,35 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Service.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package com.deadline.ShopMarketMVC.Service;
 
-import com.deadline.ShopMarketMVC.Exception.AppException;
 import com.deadline.ShopMarketMVC.Model.Customers;
-import com.deadline.ShopMarketMVC.Repository.CustomerRepository;
-import java.util.List;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Admin
  */
-@Service
-public class CustomerService {
-    @Autowired
-    private CustomerRepository customerRepository ;
+public interface CustomerService {
+    Customers save(Customers customer);
     
-    public List<Customers> getCustomerList() {
-        List<Customers> customers = customerRepository.findAll();
-        return customers;
-    }
-    
-    public Customers getCustomerByID(Integer id) {
-        Optional<Customers> customer = customerRepository.findById(id);
-        if(customer.isEmpty())
-            throw new AppException(404,"Customers not found");
-        return customer.get();
-    }
 }
---
+++ src/main/java/com/deadline/ShopMarketMVC/Service/CustomerServiceImpl.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Service.java to edit this template
+ */
+package com.deadline.ShopMarketMVC.Service;
+
+import com.deadline.ShopMarketMVC.Exception.AppException;
+import com.deadline.ShopMarketMVC.Model.Customers;
+import com.deadline.ShopMarketMVC.Repository.CustomerRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Admin
+ */
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository ;
+    
+    public List<Customers> getCustomerList() {
+        List<Customers> customers = customerRepository.findAll();
+        return customers;
+    }
+    
+    public Customers getCustomerByID(Integer id) {
+        Optional<Customers> customer = customerRepository.findById(id);
+        if(customer.isEmpty())
+            throw new AppException(404,"Customers not found");
+        return customer.get();
+    }
+    
+    public boolean checkLogin(String username, String password) {
+        Customers customer = customerRepository.findByFullnameAndPassword(username, password);
+        
+        return customer != null;
+    }
+
+    @Override
+    public Customers save(Customers customer) {
+        return customerRepository.save(customer);
+    }
+}
