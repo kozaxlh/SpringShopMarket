d725d8f190aaf634e8457eddf0d275ad484436ab
Admin <Admin@Admin-PC>
2023-05-10 20:13:25 +0700

    add order service

---
+++ src/main/java/com/deadline/ShopMarketMVC/Service/OrderServiceImpl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Service.java to edit this template
+ */
+package com.deadline.ShopMarketMVC.Service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Admin
+ */
+@Service
+public class OrderServiceImpl implements OrderService {
+    
+}
