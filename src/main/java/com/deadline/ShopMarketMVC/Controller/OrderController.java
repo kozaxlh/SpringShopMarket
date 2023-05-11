f56aef19d56d7b7b25992422b86fff5c2bf1246f
Admin <Admin@Admin-PC>
2023-05-11 22:50:33 +0700

    add Order Controller

--- src/main/java/com/deadline/ShopMarketMVC/Controller/OrderController.java
+++ src/main/java/com/deadline/ShopMarketMVC/Controller/OrderController.java
@@ -15,10 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OrderController {
     
-    @RequestMapping("/url")
-    public String page(Model model) {
-        model.addAttribute("attribute", "value");
-        return "view.name";
-    }
+//    @RequestMapping("/url")
+//    public String page(Model model) {
+//        model.addAttribute("attribute", "value");
+//        return "view.name";
+//    }
     
 }
