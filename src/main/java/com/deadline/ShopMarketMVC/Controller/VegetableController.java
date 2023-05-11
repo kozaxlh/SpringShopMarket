e7acd3eed670c2ed6a1d88337aa29be898e5c556
Admin <Admin@Admin-PC>
2023-05-11 22:52:51 +0700

    handle searchVegetable Controller

--- src/main/java/com/deadline/ShopMarketMVC/Controller/VegetableController.java
+++ src/main/java/com/deadline/ShopMarketMVC/Controller/VegetableController.java
@@ -4,13 +4,17 @@
  */
 package com.deadline.ShopMarketMVC.Controller;
 
+import com.deadline.ShopMarketMVC.DTO.VegetableSearch;
 import com.deadline.ShopMarketMVC.Model.Vegetable;
+import com.deadline.ShopMarketMVC.Service.CategoryServiceImpl;
 import com.deadline.ShopMarketMVC.Service.VegetableServiceImpl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -21,10 +25,25 @@ public class VegetableController {
     @Autowired
     private VegetableServiceImpl vegetableService;
     
+    @Autowired
+    private CategoryServiceImpl categoryService;
+    
     @RequestMapping(value = "/vegetable")
     public String loadVegetableList(Model model) {
         List<Vegetable> list = vegetableService.getVegetableList();
+        
+        model.addAttribute("categoryList", categoryService.getCategoryList());
+        model.addAttribute("vegetableList", list);
 
+        return "vegetable";
+    }
+    
+    @RequestMapping(value = "/searchvegetable", method = RequestMethod.GET)
+    public String searchVegetable(@ModelAttribute("search") VegetableSearch search, Model model) {
+        System.out.println(search.toString());
+        List<Vegetable> list = vegetableService.searchVegetableList(search);
+        
+        model.addAttribute("categoryList", categoryService.getCategoryList());
         model.addAttribute("vegetableList", list);
 
         return "vegetable";
