e62ab5c5d1c6457968550115b140d209f66168d0
Admin <Admin@Admin-PC>
2023-05-11 22:49:49 +0700

    add Category Controller

--- src/main/java/com/deadline/ShopMarketMVC/Controller/CategoryController.java
+++ src/main/java/com/deadline/ShopMarketMVC/Controller/CategoryController.java
@@ -15,10 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CategoryController {
     
-    @RequestMapping("/url")
-    public String page(Model model) {
-        model.addAttribute("attribute", "value");
-        return "view.name";
-    }
+//    @RequestMapping("/url")
+//    public String page(Model model) {
+//        model.addAttribute("attribute", "value");
+//        return "view.name";
+//    }
     
 }
