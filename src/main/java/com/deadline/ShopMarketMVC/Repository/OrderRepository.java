f24f2f61babf666f7189f76e5577d7cf254ffb89
Admin <Admin@Admin-PC>
2023-05-15 20:28:50 +0700

    add method

--- src/main/java/com/deadline/ShopMarketMVC/Repository/OrderRepository.java
+++ src/main/java/com/deadline/ShopMarketMVC/Repository/OrderRepository.java
@@ -6,6 +6,7 @@ package com.deadline.ShopMarketMVC.Repository;
 
 import com.deadline.ShopMarketMVC.Model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -14,5 +15,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
-    
+    @Query(value = "SELECT max(O.orderID) FROM Shopmarket.Order O", nativeQuery = true)
+    Integer getLastInsertedID();
 }
