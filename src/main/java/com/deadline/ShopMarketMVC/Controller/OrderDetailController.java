cbdfca7367cb4abef886c2fe6776e6f2a4de906f
Admin <Admin@Admin-PC>
2023-05-16 01:17:51 +0700

    update Cart handle

--- src/main/java/com/deadline/ShopMarketMVC/Controller/OrderDetailController.java
+++ src/main/java/com/deadline/ShopMarketMVC/Controller/OrderDetailController.java
@@ -15,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -66,6 +67,12 @@ public class OrderDetailController {
     @RequestMapping(value = "/delete/{id}")
     public String clearCart(@PathVariable("id") Integer id) {
         orderDetailService.remove(id);
+        return "redirect:/shoppingcart";
+    }
+    
+    @RequestMapping(value = "/update")
+    public String updateCart(@RequestParam("id") Integer id, @RequestParam("quantity") short quantity) {
+        orderDetailService.update(id, quantity);
         return "redirect:/shoppingcart";
     }
 }
